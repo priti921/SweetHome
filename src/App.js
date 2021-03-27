fb36fe0bd5de700bb414e5ed274da9afe19ac9f7
priti <roypriti921@gmail.com>
2021-03-27 14:16:58 +0530

    added nav bar and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import main from './css/main.css'
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div class="Navbar">
+            <div class="menu-btn">
+                <div class="menu-btn_burger"></div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar

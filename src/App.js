199fe960c44655a9ba679d630fd3c34f62638bda
priti <roypriti921@gmail.com>
2021-04-14 13:49:39 +0530

    fixed sidebar issue and working on landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import './css/main.css'
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import home from './pages/home'
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
       <Router>
         <Navbar/>
         <Switch>
-          <Route path='/' />
+          <Route path='/' component={home}/>
         </Switch>
+
       </Router>
     </div>
     
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function home() {
+    return (
+        <div className="landing-page">
+            <p className="landing-text">Homemade cakes to order</p>
+            <button className="landing-button">choose a cake</button>
+        </div>
+    )
+}
+
+export default home

8b58ce4006fc91c1e167c24be6cf543e04e9d09d
priti <roypriti921@gmail.com>
2021-04-14 10:09:44 +0530

    working on sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import main from './css/main.css'
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
+    
     <div className="App">
-     <Navbar/>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/' />
+        </Switch>
+      </Router>
     </div>
+    
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,24 @@ import React , {useState}from 'react'
 
 
 function Navbar() {
-    // const menubtn = document.querySelector('.menu-btn');
-    // let menuOpen = false;
-    // menubtn.addEventListener('click', ()=> {
-    //     if(!menuOpen) {
-    //         menubtn.classList.add('open');
-    //         menuOpen = true;
-    //     } else {
-    //         menubtn.classList.remove('open');
-    //         menuOpen = false;
-    //     }
-    // })
+   
 
     const [menuOpen, setmenuOpen] = useState(false)
 
    
     return (
+        <>
         <div className="Navbar">
             <div className={`menu-btn ${menuOpen ? "open" : ""}`} onClick={() => setmenuOpen(!menuOpen)}>
                 <div className="menu-btn_burger"></div>
             </div>
-
         </div>
+        <nav className={`sideber ${menuOpen ? "open" : ""}`} >
+            <ul className='sidebar-items'>
+                
+            </ul>
+        </nav>
+        </>
     )
 }
 

1bee89ac0a46726c3c79e98478b37ca15366f8b2
priti <roypriti921@gmail.com>
2021-04-19 14:20:30 +0530

    adding svgs

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import leftSvg from '../images/SVG/LeftSvg.svg'
 
 function home() {
     return (
         <div className="landing-page">
             <p className="landing-text">Homemade cakes to order</p>
-            
 
             <div className="landing-button" >
                 <button >choose a cake</button>
              </div>
+             <img className="left-svg" src={leftSvg} alt="left-svg"></img>
         </div>
     )
 }

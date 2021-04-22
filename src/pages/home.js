3fbdb3dfd3378027e7c2c6bfed8b29b5af525c3c
priti <roypriti921@gmail.com>
2021-04-22 14:12:42 +0530

    working on svg

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import leftSvg from '../images/SVG/LeftSvg.svg'
+import leftSvg from '../images/SVG/LeftSvg.svg';
+import rightSvg from '../images/SVG/RightSvg2.svg';
 
 function home() {
     return (
         <div className="landing-page">
-            <p className="landing-text">Homemade cakes to order</p>
+            <div className="landing-page-details">
+                <p className="landing-text">Homemade cakes to order</p>
 
-            <div className="landing-button" >
-                <button >choose a cake</button>
-             </div>
+                <div className="landing-button" >
+                    <button >choose a cake</button>
+                </div>
+            </div>
+
              <img className="left-svg" src={leftSvg} alt="left-svg"></img>
+             <img className="right-svg" src={rightSvg} alt="right-svg"></img>
         </div>
     )
 }
